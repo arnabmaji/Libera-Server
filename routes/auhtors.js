5a8095fcb0cc920f108dcdf99a283c9d8196db37
Arnab Maji <arnabmaji1999@gmail.com>
2020-05-24 15:13:00 +0530

    add auth to get authors route

--- routes/auhtors.js
+++ routes/auhtors.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const librarianAuth = require('../middleware/librarian-auth');
 const {getAuthors} = require('../models/author');
 
 const router = express.Router();
 
 // route for fetching all authors
-router.get('/', async (req, res) => {
+router.get('/', librarianAuth, async (req, res) => {
     // TODO: add auth middleware
     res
         .status(200)
