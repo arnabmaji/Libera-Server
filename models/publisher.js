75aeab095aadf1423d74cc7cfa4cfa45621b3484
Arnab Maji <arnabmaji1999@gmail.com>
2020-05-26 13:49:59 +0530

    add route for creating new publishers

--- models/publisher.js
+++ models/publisher.js
@@ -1,9 +1,31 @@
 const database = require('./db');
+const Joi = require('joi');
 
 async function getPublishers() {
     return await database.query('SELECT * FROM publishers');
 }
 
+async function addPublisher(publisher) {
+    /*
+    * Create new Publisher in the database
+     */
+    const result = await database.query('INSERT INTO publishers SET ?', publisher);
+    return result.affectedRows === 1;
+}
+
+function validatePublisherParams(publisher) {
+    /*
+    * Validate publisher params received in request body
+     */
+
+    const schema = {
+        name: Joi.string().min(4).required()
+    }
+    return Joi.validate(publisher, schema);
+}
+
 module.exports = {
-    getPublishers
+    getPublishers,
+    validatePublisherParams,
+    addPublisher
 }
--- routes/publishers.js
+++ routes/publishers.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const librarianAuth = require('../middleware/librarian-auth');
+const _ = require('lodash');
 
 const router = express.Router();
-const {getPublishers} = require('../models/publisher');
+const {getPublishers, validatePublisherParams, addPublisher} = require('../models/publisher');
 
 // add route for fetching all publishers
 router.get('/', librarianAuth, async (req, res) => {
     res
         .status(200)
         .send(await getPublishers());
+});
+
+// add route for adding new publishers
+router.post('/', librarianAuth, async (req, res) => {
+
+    // retrieve publisher params from request body
+    let publisher = _.pick(req.body, 'name');
+    // validate params
+    const {error} = validatePublisherParams(publisher);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const result = await addPublisher(publisher);
+    res.sendStatus(result ? 200 : 500);
 });
 
 module.exports = router;
